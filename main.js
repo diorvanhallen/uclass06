d1cc7e0ce06166f14d0dcbbfff8f1dabb3f01694
TANIA JAQUEZ <tajaquez.c02@ucamp.utel.edu.mx>
2021-09-11 19:55:52 -0600

    arrays

---
+++ main.js
@@ -0,0 +1,12 @@
+var arreglo = [1,"cadena de texto", true];
+console.log(arreglo);
+
+function promedio (calificaciones){
+    console.log(calificaciones);
+    console.log (calificaciones.lengh);
+
+    console.log(calificaciones[3])
+    Array[3] = "cadena"
+}
+
+promedio ([90, 78, 90, 40]);
